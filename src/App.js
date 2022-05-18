ea3ce122512716c2be61e7d8646a003a9a713b98
Thowhid31 <thowhidantor16@gmail.com>
2022-05-19 02:44:27 +0600

    authentication implement

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Pages/Header';
 import Home from './Pages/Home/Home';
+import RequireAuth from './Pages/RequireAuth';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/todo' element={<Header></Header>}></Route>
+        <Route path='/todo' element={<RequireAuth>
+          <Header></Header>
+        </RequireAuth>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import auth from '../firebase.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import {useLocation, useNavigate } from 'react-router-dom';
+import auth from '../firebase.init';
 
 const Login = () => {
-    const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, user, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+    let errorElementOwner;
+    if(error){
+        errorElementOwner = 
+            <div>
+                <p className='text-danger'>Error: {error.message}</p>
+            </div>
+    }
+ 
+    if(user){
+        navigate(from, {replace: true})
+    }
     return (
         <div>
-            <h1>Login First for Go To Do List</h1>
-            <button
-                        onClick={() => signInWithGoogle()}
-                        className="todo-class3">Continue with Google</button>
+        {errorElementOwner}
+            <div>       
+                <button 
+                onClick={()=> signInWithGoogle()}
+                className='btn btn-danger google-button d-block mx-auto'>
+                    Sign In with Google</button>
+            </div>
         </div>
     );
 };
