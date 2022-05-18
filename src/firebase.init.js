36c5fa4ebf6e031f95b6b253acfc9a7c7919b315
Thowhid31 <thowhidantor16@gmail.com>
2022-05-19 05:19:57 +0600

    may this last commit

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyADvLqykhbR41_iS8vrr8_Ftnd0y3f301s",
-  authDomain: "to-do-list-app-c04ed.firebaseapp.com",
-  projectId: "to-do-list-app-c04ed",
-  storageBucket: "to-do-list-app-c04ed.appspot.com",
-  messagingSenderId: "141486703824",
-  appId: "1:141486703824:web:a32cb3cd4e934117eccad5"
+  apiKey: process.env.REACT_APP_API_KEY, 
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN, 
+  projectId: process.env.REACT_APP_PROJECT_ID, 
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET, 
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID, 
+  appId: process.env.REACT_APP_APP_ID, 
 };
 
 // Initialize Firebase
